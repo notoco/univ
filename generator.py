95cc5e968cf9a9ef0caa6bd928c54c3dd139b79c
notoco <36659110+notoco@users.noreply.github.com>
2019-03-16 21:15:56 +0100

    Przewodnik PVR  - poprawki

--- generator.py
+++ generator.py
@@ -162,15 +162,16 @@ class GeneratorZIP:
         if not os.path.exists("zip"):
             os.makedirs("zip")
         folder = "zip"
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path): shutil.rmtree(file_path)
-            except Exception as e:
-                print(e)
-                continue
+# Pozostawienie starych wersji w katalogu
+#       for the_file in os.listdir(folder):
+#           file_path = os.path.join(folder, the_file)
+#           try:
+#               if os.path.isfile(file_path):
+#                   os.unlink(file_path)
+#               elif os.path.isdir(file_path): shutil.rmtree(file_path)
+#           except Exception as e:
+#                print(e)
+#                continue
         addons = os.listdir( "." )
         for addon in addons:
             try:
